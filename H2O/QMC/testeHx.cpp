08e135efca664d01cea852da08f270cd513517d6
Micha <goette@math.tu-berlin.de>
2020-03-23 16:02:19 +0100

    update

--- H2O/QMC/testeHx.cpp
+++ H2O/QMC/testeHx.cpp
@@ -22,8 +22,8 @@ int main(){
 	read_from_disc(path_T2,T);
 	read_from_disc(path_V2,V);
 
-	size_t test_number = 3;
-	size_t test_number2 = 100;
+	size_t test_number = 0;
+	size_t test_number2 = 0;
 	std::vector<size_t> sample = {0,1,2,3,22,23,30,31};
 
 	XERUS_LOG(info, "--- Loading shifted and preconditioned Hamiltonian ---");
@@ -42,6 +42,23 @@ int main(){
 
 
 	ContractPsiHek builder(phi,d,p,path_T,path_V,0.0, shift);
+
+	size_t pp = 2, qq =4, rr = 2, ss = 0;
+	std::vector<size_t> idx = {pp,qq,rr,ss};
+	XERUS_LOG(info,	builder.returnVValue(pp/2,qq/2,rr/2,ss/2) << " " << V[{pp,qq,rr,ss}]); //2030
+	XERUS_LOG(info,	builder.returnVValue(rr/2,qq/2,pp/2,ss/2) << " " << V[{rr,qq,pp,ss}]); //3020
+	XERUS_LOG(info,	builder.returnVValue(pp/2,ss/2,rr/2,qq/2) << " " << V[{pp,ss,rr,qq}]);
+	XERUS_LOG(info,	builder.returnVValue(rr/2,ss/2,pp/2,qq/2) << " " << V[{rr,ss,pp,qq}]);
+	XERUS_LOG(info,	builder.returnVValue(qq/2,pp/2,ss/2,rr/2) << " " << V[{qq,pp,ss,rr}]);
+	XERUS_LOG(info,	builder.returnVValue(ss/2,pp/2,qq/2,rr/2) << " " << V[{ss,pp,qq,rr}]);
+	XERUS_LOG(info,	builder.returnVValue(qq/2,rr/2,ss/2,pp/2) << " " << V[{qq,rr,ss,pp}]);
+	XERUS_LOG(info,	builder.returnVValue(ss/2,rr/2,qq/2,pp/2) << " " << V[{ss,rr,qq,pp}]);
+
+	XERUS_LOG(info,	builder.returnTValue(pp,rr) << " " << T[{pp,rr}]);
+	XERUS_LOG(info,	builder.returnTValue(rr,pp) << " " << T[{rr,pp}]);
+
+
+
 	for (size_t i = 0; i< test_number; ++i){
 		builder.reset(sample);
 		value_t val1 = builder.contract();
