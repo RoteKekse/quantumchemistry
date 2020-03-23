2049eaa4dd90e70205f8c3abc5525c8c15de045e
Micha <goette@math.tu-berlin.de>
2020-03-23 14:13:58 +0100

    update

--- classes/QMC/contractpsihek.cpp
+++ classes/QMC/contractpsihek.cpp
@@ -163,7 +163,7 @@ class ContractPsiHek{
 				return V[{i,k,j,l}];
 			if (i <= k && j<= k && l <= (j==k ? i : j))
 				return V[{k,i,j,l}];
-			if (l <= i && k<= i && j <= (i==k ? l : k))
+			if (k <= i && l<= i && j <= (i==l ? k : l))
 				return V[{i,k,l,j}];
 			if (l <= k && i<= k && j <= (i==k ? l : i))
 				return V[{k,i,l,j}];
