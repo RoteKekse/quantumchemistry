88069eacb6cc5a84fd65a622a3bef3f349f7f646
Micha <goette@math.tu-berlin.de>
2020-03-23 16:16:31 +0100

    update

--- classes/QMC/contractpsihek.cpp
+++ classes/QMC/contractpsihek.cpp
@@ -163,7 +163,7 @@ class ContractPsiHek{
 		}
 
 		value_t returnVValue(size_t i, size_t k, size_t j, size_t l){
-			XERUS_LOG(info, i<<j<<k<<l );
+			//XERUS_LOG(info, i<<j<<k<<l );
 			if (j <= i){
 				if (k<= i && l <= (i==k ? j : k))
 					return V[{i,j,k ,l}];
