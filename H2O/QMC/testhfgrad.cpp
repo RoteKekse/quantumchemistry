053be533de5f8831cbd1e5180d56b61fc1f16e16
Micha <goette@math.tu-berlin.de>
2020-03-27 11:43:47 +0100

    update

--- H2O/QMC/testhfgrad.cpp
+++ H2O/QMC/testhfgrad.cpp
@@ -4,11 +4,12 @@
 #include "../../classes/loading_tensors.cpp"
 #include "../../classes/helpers.cpp"
 #include "../../classes/QMC/contractpsihek.cpp"
+#include "../../classes/QMC/tangential_parallel.cpp"
 
 
 int main(){
 	Index i1,i2,i3,j1,j2,j3,k1,k2;
-	size_t d = 48,p = 8;
+	size_t d = 48,p = 8, iterations=5e5;;
 	value_t shift = 25.0;
 	std::vector<size_t> hf_sample = {0,1,2,3,22,23,30,31};
 	std::string path_T = "../data/T_H2O_48_bench_single.tensor";
@@ -18,6 +19,7 @@ int main(){
 
 	ContractPsiHek builder(ehf,d,p,path_T,path_V,0.0, shift);
 	builder.reset(hf_sample);
+	Tangential tang(d,p,iterations,path_T,path_V,shift,hf_sample,ehf);
 
 	xerus::TTOperator Hs;
 	std::string name2 = "../data/hamiltonian_H2O_" + std::to_string(d)  +"_full_shifted_benchmark.ttoperator";
@@ -43,35 +45,39 @@ int main(){
 	XERUS_LOG(info,grad_test.ranks());
 	XERUS_LOG(info,"Error = " << (grad_test-grad).frob_norm());
 
-	auto step1 = ehf - 0.3 *grad_test;
-	auto step2 = ehf - 0.2 *grad_test;
-	auto step3 = ehf - 0.1 *grad_test;
-	auto step4 = ehf - 0.05 *grad_test;
-	step1 /= step1.frob_norm();
-	step2 /= step2.frob_norm();
-	step3 /= step3.frob_norm();
-	step4 /= step4.frob_norm();
-	XERUS_LOG(info,"Start ev " << lambda);
-	XERUS_LOG(info,"Step1    " <<contract_TT(Hs,step1,step1));
-	XERUS_LOG(info,"Step2    " <<contract_TT(Hs,step2,step2));
-	XERUS_LOG(info,"Step3    " <<contract_TT(Hs,step3,step3));
-	XERUS_LOG(info,"Step4    " <<contract_TT(Hs,step4,step4));
+//	auto step1 = ehf - 0.3 *grad_test;
+//	auto step2 = ehf - 0.2 *grad_test;
+//	auto step3 = ehf - 0.1 *grad_test;
+//	auto step4 = ehf - 0.05 *grad_test;
+//	step1 /= step1.frob_norm();
+//	step2 /= step2.frob_norm();
+//	step3 /= step3.frob_norm();
+//	step4 /= step4.frob_norm();
+//	XERUS_LOG(info,"Start ev " << lambda);
+//	XERUS_LOG(info,"Step1    " <<contract_TT(Hs,step1,step1));
+//	XERUS_LOG(info,"Step2    " <<contract_TT(Hs,step2,step2));
+//	XERUS_LOG(info,"Step3    " <<contract_TT(Hs,step3,step3));
+//	XERUS_LOG(info,"Step4    " <<contract_TT(Hs,step4,step4));
 
 	size_t rank = 20;
+	auto step1 = ehf - 0.2 *grad;
+	auto step2 = ehf - 0.1 *grad;
+	auto step3 = ehf - 0.05 *grad;
+
 	step1.round(rank);
 	step2.round(rank);
 	step3.round(rank);
-	step4.round(rank);
 
 	step1 /= step1.frob_norm();
 	step2 /= step2.frob_norm();
 	step3 /= step3.frob_norm();
-	step4 /= step4.frob_norm();
-	XERUS_LOG(info,"Start ev " << lambda);
-	XERUS_LOG(info,"Step1    " <<contract_TT(Hs,step1,step1));
-	XERUS_LOG(info,"Step2    " <<contract_TT(Hs,step2,step2));
-	XERUS_LOG(info,"Step3    " <<contract_TT(Hs,step3,step3));
-	XERUS_LOG(info,"Step4    " <<contract_TT(Hs,step4,step4));
+	tang.update(step1);
+	XERUS_LOG(info,"Step1    " <<tang.get_eigenvalue() << "\n" << step1.ranks());
+	tang.update(step2);
+	XERUS_LOG(info,"Step2    " <<tang.get_eigenvalue() << "\n" << step2.ranks());
+	tang.update(step3);
+	XERUS_LOG(info,"Step3    " <<tang.get_eigenvalue() << "\n" << step3.ranks());
+
 
 	return 0;
 }
