149e68a8ec5bf78d66f1c60f3b0b183a65c5b641
Micha <goette@math.tu-berlin.de>
2020-03-27 09:48:43 +0100

    update

--- N2/QMC/testhfgrad.cpp
+++ N2/QMC/testhfgrad.cpp
@@ -9,7 +9,7 @@
 
 int main(){
 	Index i1,i2,i3,j1,j2,j3,k1,k2;
-	size_t d = 120,p = 14,rank = 30, iterations=5e5;
+	size_t d = 120,p = 14,rank = 60, iterations=5e5;
 	value_t shift = 135.0;
 
 	std::vector<size_t> hf_sample = {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
