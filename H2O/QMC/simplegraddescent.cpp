0f870457f375c2e52a7d8dfb5f35ec21fca164f2
Micha <goette@math.tu-berlin.de>
2020-04-16 13:29:31 +0200

    update

--- H2O/QMC/simplegraddescent.cpp
+++ H2O/QMC/simplegraddescent.cpp
@@ -14,26 +14,30 @@ int main(){
 	std::string path_V= "../data/V_H2O_48_bench_single.tensor";
 	std::vector<size_t> sample = { 0, 1,2,3,22,23,30,31 };
 	value_t nuc = -52.4190597253;
+	value_t alpha = 0.01;
 
 	XERUS_LOG(info,"Loading Start vector from disc");
-	TTTensor phi,res;
-	read_from_disc("../data/hf_gradient_48.tttensor",phi);
+	TTTensor start, phi,res;
+	read_from_disc("../data/hf_gradient_48.tttensor",start);
+	phi = makeUnitVector(sample,2*nob);
 
 	xerus::TTOperator Hs;
 	std::string name2 = "../data/hamiltonian_H2O_" + std::to_string(2*nob)  +"_full_shifted_benchmark.ttoperator";
 	read_from_disc(name2,Hs);
 
 	XERUS_LOG(info,"Round start vector to " << eps << " keepin gisng values bigger than " << eps/std::sqrt(2*nob-1));
-	phi/= phi.frob_norm();
-	phi.round(eps);
-	phi/= phi.frob_norm();
+	start/= start.frob_norm();
+	start.round(eps);
+	start/= start.frob_norm();
+	phi -= alpha*start;
+	phi/=phi.frob_norm();
+	XERUS_LOG(info,phi.ranks());
 
 	Tangential tang(2*nob,num_elec,iterations,path_T,path_V,shift,sample,phi);
 	TangentialOperation top(phi);
 	tang.uvP.xbase.first = top.xbasis[0]; //use same orthogonalization!!!
 	tang.uvP.xbase.second = top.xbasis[1];
 
-	value_t alpha = 0.01;
 	for (size_t i = 0; i < numIter;++i){
 		ev_app = tang.get_eigenvalue();
 		XERUS_LOG(info, "Eigenvalue approx. " << ev_app - shift +nuc);
@@ -43,7 +47,12 @@ int main(){
 		res /= res.frob_norm();
 
 		phi -= alpha*res;
+
 		phi /= phi.frob_norm();
+		phi.round(eps);
+		phi /= phi.frob_norm();
+		XERUS_LOG(info,phi.ranks());
+
 		//update
 		top.update(phi);
 		tang.update(phi);
