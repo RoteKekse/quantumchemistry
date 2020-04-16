859e4da62142b64c2b2275219131b904bcb2fe62
Micha <goette@math.tu-berlin.de>
2020-04-16 13:25:32 +0200

    update

--- classes/QMC/tangential_parallel.cpp
+++ classes/QMC/tangential_parallel.cpp
@@ -134,62 +134,6 @@ class Tangential{
 	return results;
 	}
 
-		std::vector<Tensor> get_2_tangential_components(value_t ev, size_t start_pos, value_t accuracy=0.0001){
-			value_t ev_exact,res,psi_ek,factor,dk;
-			std::vector<Tensor> results; // Vector of Tangential space components
-			xerus::TTOperator Hs,Fock_inv;
-		  std::string name2 = "/homes/numerik/goette/Documents/eclipse-workspace/Xerus_Sandbox/QC-Experiments/H2O/data/hamiltonian_H2O_" + std::to_string(d)  +"_full_shifted_benchmark.ttoperator";
-			read_from_disc(name2,Hs);
-			name2 = "../data/fock_h2o_inv_shifted" + std::to_string(d) +"_full.ttoperator";
-			read_from_disc(name2,Fock_inv);
-
-			//for (size_t pos = start_pos; pos < start_pos+1; ++pos){
-			for (size_t pos = 0; pos < d; ++pos){
-				value_t prob = 0, prob2  = 0;
-				ProjectorProbabilityFunction3 PPF(phi,pos, true,Fock_inv);
-				Metropolis<ProjectorProbabilityFunction3> markow2(&PPF,  /*TODO check*/ TrialSample, start_sample, d);
-
-				std::unordered_map<std::vector<size_t>,std::pair<size_t,value_t>,container_hash<std::vector<size_t>>> samples;
-				runMetropolis<ProjectorProbabilityFunction3>(&markow2,samples,iterations);
-
-				size_t count = 0;
-				Tensor result(phi.component(pos).dimensions);
-				for (std::pair<std::vector<size_t>,std::pair<size_t,value_t>> const& pair: samples) {
-					prob +=pair.second.second; //TODO check this
-					if ((value_t) pair.second.first > accuracy * (value_t) iterations){
-						prob2 +=pair.second.second; //TODO check this
-						count++;
-						auto itr = eHxValues.find(pair.first);
-						if (itr == eHxValues.end()){
-							 //setting builder to newest sample!! Important
-							builder.reset(pair.first);
-							builder.preparePsiEval();
-							eHxValues[pair.first] = builder.contract_tree();
-						}
-
-						auto loc_grad = uvP.localProduct(pair.first,pos,true);
-						auto idx = makeIndex(pair.first);
-
-						builder.reset(pair.first);
-						//dk = builder.diagionalEntry();
-						//factor = (eHxValues[pair.first] - ev*phi[idx])/dk;
-
-						auto idx2 = makeIndex(pair.first);
-						idx2.resize(2 * d);
-						std::copy_n(idx2.begin(), d, idx2.begin() + d);
-						value_t dk = Fock_inv[idx2];
-						factor = (eHxValues[pair.first] - ev*phi[idx])*dk;
-
-						result += factor * loc_grad;
-					}
-				}
-				XERUS_LOG(info,"Pos = " << pos << " " << result.frob_norm() << " Number of samples " << samples.size() << " Prob " << prob << " Prob2 " << prob2);
-				//XERUS_LOG(info,result);
-				results.emplace_back(result);
-			}
-		return results;
-	}
-
 
 	TTTensor builtTTTensor(const std::vector<Tensor>& y){
 		time_t begin_time = time (NULL);
