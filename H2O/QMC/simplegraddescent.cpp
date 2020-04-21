5105e7b0d7ee22ccc3f6916584b50b04edb16fca
Micha <goette@math.tu-berlin.de>
2020-04-21 08:28:55 +0200

    update

--- H2O/QMC/simplegraddescent.cpp
+++ H2O/QMC/simplegraddescent.cpp
@@ -9,15 +9,15 @@
 
 int main(){
 	size_t nob = 24,num_elec = 8,iterations = 1e4,pos = 5, numIter = 20,rank=10;
-	value_t ev, shift = 25.0, ev_app, ev_ex, eps=1.5;
+	value_t ev, shift = 25.0, ev_app, ev_ex, eps=1.5,ev_app_tmp;
 	std::string path_T = "../data/T_H2O_48_bench_single.tensor";
 	std::string path_V= "../data/V_H2O_48_bench_single.tensor";
 	std::vector<size_t> sample = { 0, 1,2,3,22,23,30,31 };
 	value_t nuc = -52.4190597253;
-	value_t alpha = 0.01;
+	value_t alpha = 0.1;
 
 	XERUS_LOG(info,"Loading Start vector from disc");
-	TTTensor start, phi,res;
+	TTTensor start, phi,res, phi_tmp;
 	read_from_disc("../data/hf_gradient_48.tttensor",start);
 	phi = makeUnitVector(sample,2*nob);
 
@@ -39,26 +39,41 @@ int main(){
 	TangentialOperation top(phi);
 	tang.uvP.xbase.first = top.xbasis[0]; //use same orthogonalization!!!
 	tang.uvP.xbase.second = top.xbasis[1];
+	ev_app = tang.get_eigenvalue();
 	for (size_t i = 0; i < numIter;++i){
-		ev_app = tang.get_eigenvalue();
 		XERUS_LOG(info, "Eigenvalue approx. " << std::setprecision(8) << ev_app - shift +nuc);
 		XERUS_LOG(info, "Eigenvalue exact   " << std::setprecision(8) << contract_TT(Hs,phi,phi)- shift +nuc);
 		auto tang_app = tang.get_tangential_components(ev_app,0.005);
 		res = top.builtTTTensor(tang_app);
 		res /= res.frob_norm();
 
-		phi -= alpha*res;
+		ev_app_tmp = ev_app;
+		while (true){
+			phi_tmp = phi -  alpha*res;
 
-		phi /= phi.frob_norm();
-		phi.round(rank);
-		phi /= phi.frob_norm();
-		XERUS_LOG(info,phi.ranks());
+			phi_tmp /= phi_tmp.frob_norm();
+			phi_tmp.round(rank);
+			phi_tmp /= phi_tmp.frob_norm();
+			tang.update(phi_tmp);
+			ev_app_tmp = tang.get_eigenvalue();
+			if (ev_app_tmp < ev_app or alpha < 1e-3)
+				break;
+			else
+				alpha /=2;
+		}
+		if (alpha < 1e-3)
+			break;
+		phi = phi_tmp;
+		ev_app = ev_app_tmp;
+		XERUS_LOG(info, "alpha = " << alpha);
+		XERUS_LOG(info,"ranks = \n" <<phi.ranks());
 
 		//update
 		top.update(phi);
 		tang.update(phi);
 		tang.uvP.xbase.first = top.xbasis[0]; //use same orthogonalization!!!
 		tang.uvP.xbase.second = top.xbasis[1];
+		alpha *= 2;
 	}
 
 
