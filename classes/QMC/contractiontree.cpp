b7bfe1d1f3d1931eca587dcd3a4e593861edaa41
Micha <goette@math.tu-berlin.de>
2020-04-27 12:35:47 +0200

    update

--- classes/QMC/contractiontree.cpp
+++ classes/QMC/contractiontree.cpp
@@ -42,9 +42,10 @@ class ContractionTree {
 				XERUS_LOG(info, "l = " << l);
 				std::vector<Tensor> list_tmp;
 				size_t s = tree[l-1].size() / 2;
-				XERUS_LOG(info,"s = " << s);
+				XERUS_LOG(info,"  s = " << s);
 				for (size_t c = 0; c < s; ++s){
-					XERUS_LOG(info, "  c = " << c);
+					XERUS_LOG(info, "    c = " << c);
+					XERUS_LOG(info, tree[l-1][0]);
 					Tensor tmp;
 					tmp(r1,r2) = tree[l-1][2*s](r1,r3) * tree[l-1][2*s+1](r3,r2);
 					list_tmp.emplace_back(std::move(tmp));
