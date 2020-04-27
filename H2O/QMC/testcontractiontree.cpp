fde957b52bdb091d7074bf5bd705fb8f1add6550
Micha <goette@math.tu-berlin.de>
2020-04-27 12:18:00 +0200

    update

--- H2O/QMC/testcontractiontree.cpp
+++ H2O/QMC/testcontractiontree.cpp
@@ -10,11 +10,12 @@ using xerus::misc::operator<<;
 
 int main(){
 	size_t d = 48, p = 8;
-	std::vector<size_t> hf_sample = {1,2,3,22,23,30,21};
+	std::vector<size_t> hf_sample = {1,2,3,4,22,23,30,31};
 	xerus::TTTensor phi,res,res_last;
 	read_from_disc("../data/eigenvector_H2O_48_3_-23.647510_benchmark.tttensor",phi);
 
 	std::vector<size_t> hf_idx = makeIndex(hf_sample,d);
+
 	XERUS_LOG(info, "Phi at HF sample "  << phi[hf_idx]);
 
 	ContractionTree(phi,hf_sample);
