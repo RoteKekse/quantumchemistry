2e35364f5004aad57f4fde2d0bc21f8b530cc10e
Micha <goette@math.tu-berlin.de>
2020-04-29 08:51:11 +0200

    update

--- H2O/QMC/testspinupspindowninvariance.cpp
+++ H2O/QMC/testspinupspindowninvariance.cpp
@@ -3,60 +3,23 @@
 #include "../../classes/helpers.cpp"
 #include "../../classes/QMC/trialfunctions.cpp"
 
-std::pair<size_t,size_t> getUPDown(std::vector<size_t> s){
-	size_t pup=0, pdown=0;
-	for (size_t i : s){
-		if (i%2 == 0)
-			pup++;
-		else
-			pdown++;
-	}
-	return std::pair<size_t,size_t>(pup,pdown);
-}
+
 
 int main(){
 	size_t d = 48,p = 8,iterations = 1e5, rank = 10;
 	value_t ev, shift = 25.0, ev_app;
+	std::vector<size_t> hf_sample = {0,1,2,3,22,23,30,31};
 
-	TTTensor direction;
-	read_from_disc("../data/hf_gradient_48.tttensor",direction);
-	direction /= direction.frob_norm();
-	direction.round(10);
+	xerus::TTTensor phi,res,res_last;
+	read_from_disc("../data/eigenvector_H2O_48_3_-23.647510_benchmark.tttensor",phi);
 
-	auto id = xerus::TTOperator::identity(std::vector<size_t>(2*d,2));
-	XERUS_LOG(info,"Direction norm   " <<direction.frob_norm());
-	XERUS_LOG(info,"Direction ranks  " <<direction.ranks());
-	std::vector<size_t> s1({ 1, 2, 3, 11, 22, 23, 31, 37 });
-	std::vector<size_t> s2({ 1, 2, 3, 9, 22, 23, 30, 35 });
-	auto ek1 = makeUnitVector(s1,d);
-	auto ek2 = makeUnitVector(s2,d);
-	XERUS_LOG(info,getUPDown( s1) << " \t" << s1<<": \t" <<contract_TT(id,ek1,direction));
-	XERUS_LOG(info,getUPDown( s2) << " \t" << s2<<": \t" <<contract_TT(id,ek2,direction));
+	auto P = particleNumberOperator(d);
+	auto Pup = particleNumberOperatorUp(d);
+	auto Pdown = particleNumberOperatorDown(d);
 
-
-	std::vector<size_t> sample({0,1,2,3,22,23,30,31});
-	auto ehf = makeUnitVector(sample,d);
-	while(false){
-		auto ek = makeUnitVector(sample,d);
-		value_t val = contract_TT(id,ek,direction);
-		if (std::abs(val)> 1e-12)
-			XERUS_LOG(info,getUPDown( sample) << " \t" << sample<<": \t" <<contract_TT(id,ek,direction));
-		sample = TrialSample(sample,d);
-
-	}
-
-
-//
-//	std::pair<std::vector<size_t>,std::vector<size_t>> sample({0,1,11,15},{0,1,11,15});
-//	auto ehf = makeUnitVector(sample,d);
-//	while(true){
-//		auto ek = makeUnitVector(sample,d);
-//		value_t val = contract_TT(id,ek,direction);
-//		if (std::abs(val)> 1e-12)
-//			XERUS_LOG(info,sample<<": \t" <<contract_TT(id,ek,direction));
-//		sample = TrialSample(sample,d);
-//
-//	}
+	XERUS_LOG(info,"Particle number phi updated       " << std::setprecision(16) << contract_TT(P,phi,phi));
+	XERUS_LOG(info,"Particle number up phi updated    " << std::setprecision(16) << contract_TT(Pup,phi,phi));
+	XERUS_LOG(info,"Particle number down phi updated  " << std::setprecision(16) << contract_TT(Pdown,phi,phi));
 
 
 
