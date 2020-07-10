a4fc7015e0887224d841f597e22019beb3d2b0b1
Micha <goette@math.tu-berlin.de>
2020-07-10 09:58:19 +0200

    update

--- H2O/ParticleNumberOfSlices/testslicepn.cpp
+++ H2O/ParticleNumberOfSlices/testslicepn.cpp
@@ -87,11 +87,11 @@ int main(){
 //	test[{1,1,1}] = (double)rand() / RAND_MAX;
 //	test[{1,1,2}] = (double)rand() / RAND_MAX;
 //	test[{1,1,3}] = (double)rand() / RAND_MAX;
-//	test[{2,0,4}] = (double)rand() / RAND_MAX;
+	test[{2,0,4}] = (double)rand() / RAND_MAX;
 //	test[{2,0,5}] = (double)rand() / RAND_MAX;
 //	test[{2,0,6}] = (double)rand() / RAND_MAX;
 //	test[{2,1,1}] = (double)rand() / RAND_MAX;
-	test[{2,1,2}] = (double)rand() / RAND_MAX;
+//	test[{2,1,2}] = (double)rand() / RAND_MAX;
 //	test[{2,1,3}] = (double)rand() / RAND_MAX;
 	test[{3,1,4}] = (double)rand() / RAND_MAX;
 //	test[{3,1,5}] = (double)rand() / RAND_MAX;
