69eecca014e2e2c232316f66c0fca530eb0e83df
Micha <goette@math.tu-berlin.de>
2020-08-03 14:57:09 +0200

    update

--- tests/testhamiltonianrank.cpp
+++ tests/testhamiltonianrank.cpp
@@ -7,7 +7,7 @@ using xerus::misc::operator<<;
 
 int main(){
 
-	for (size_t d = 8; d < 22; d=d+2){
+	for (size_t d = 8; d < 20; d=d+2){
 		XERUS_LOG(info,d);
 		size_t idx = d/2;
 
@@ -16,31 +16,28 @@ int main(){
 		TTOperator res(std::vector<size_t>(2*d,2));
 		TTOperator res2(std::vector<size_t>(2*d,2));
 		for (size_t i = 0; i < idx; ++i){
-			for (size_t j = 0; j < idx; ++j){
+			for (size_t j = idx; j < d; ++j){
 				for (size_t k = idx; k < d; ++k){
 					for (size_t l = idx; l < d; ++l){
 						double number = distribution(generator);
-						//double number = 1;
 						res += number*return_two_e_ac(i,j,k,l,d);
-
-
 					}
 				}
 			}
 		}
-		for (size_t k = 0; k < idx; ++k){
-			for (size_t l = idx; l < d; ++l){
-				double number = distribution(generator);
-				//double number = 1;
-				res2 += number*return_one_e_ac(k,l,d);
-
-
-			}
-		}
+//		for (size_t k = 0; k < idx; ++k){
+//			for (size_t l = idx; l < d; ++l){
+//				double number = distribution(generator);
+//				//double number = 1;
+//				res2 += number*return_one_e_ac(k,l,d);
+//
+//
+//			}
+//		}
 		res.round(1e-8);
-		res2.round(1e-8);
+//		res2.round(1e-8);
 		XERUS_LOG(info,res.ranks());
-		XERUS_LOG(info,res2.ranks());
+//		XERUS_LOG(info,res2.ranks());
 	}
 
 
