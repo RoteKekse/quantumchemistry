f937ada0e7e17bd92922dcb0b57687bf87bc8285
Micha <goette@math.tu-berlin.de>
2020-09-07 10:53:39 +0200

    update

--- tests/testekHx.cpp
+++ tests/testekHx.cpp
@@ -8,11 +8,14 @@ using xerus::misc::operator<<;
 
 
 int main(){
-
+	std::string path_T = "../H2O/data/T_H2O_48_bench_single.tensor";
+	std::string path_V = "../H2O/data/V_H2O_48_bench_single.tensor";
 	size_t rank = 5,d = 48, p = 8;
+	value_t shift = 25;
 
 	TTTensor phi = TTTensor::random(std::vector<size_t>(d,2),std::vector<size_t>(d-1,rank));
 
+	ContractPsiHek builder(phi,d,p,path_T,path_V,0.0, shift);
 
 
 
