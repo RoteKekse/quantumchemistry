856162dc2ea390b7da02123489af187d94b1abe8
Micha <goette@math.tu-berlin.de>
2020-09-07 11:07:52 +0200

    update

--- tests/testekHx.cpp
+++ tests/testekHx.cpp
@@ -15,8 +15,8 @@ int main(){
 	std::vector<size_t> hf = {0,1,2,3,22,23,30,31};
 
 
-	std::valarray<size_t> linear;
-	std::valarray<size_t> tree;
+	std::vector<size_t> linear;
+	std::vector<size_t> tree;
 	for (size_t rank : {5,10,20,40,80,150}){
 	TTTensor phi = TTTensor::random(std::vector<size_t>(d,2),std::vector<size_t>(d-1,rank));
 
@@ -29,7 +29,8 @@ int main(){
 	}
 	XERUS_LOG(info, "tree " << tree);
 	XERUS_LOG(info, "line " << linear);
-	XERUS_LOG(info, "ratio " << tree/linear);
+	for (size_t i = 0; i < tree.size(); ++i)
+		XERUS_LOG(info, tree[i] / linear[i]);
 
 
 	return 0;
