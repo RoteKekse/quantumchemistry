9e07c02bd409100a4ce0d4247927da882e01a72b
Micha <goette@math.tu-berlin.de>
2020-10-21 16:03:21 +0200

    update

--- classes/QMC/contractpsihek.cpp
+++ classes/QMC/contractpsihek.cpp
@@ -416,7 +416,7 @@ class ContractPsiHek{
 														idx_new.insert(idx_new.end(),tuple2.first.begin(),tuple2.first.end());
 														Tensor tmp;
 														multiplications += tuple1.second.dimensions[0]*tuple2.second.dimensions[0];
-														tmp(r1) = tuple1.second(r1,r2)*tuple2.second(r2);
+														tmp(r1) = tuple2.second(r1,r2)*tuple1.second(r2);
 														datai[getIndex(i1+i2,j1+j2,k1+k2,l1+l2)].emplace_back(std::pair<std::vector<size_t>,Tensor>(idx_new,std::move(tmp)));
 				}}}}}}}}}}
 				data2 = datai;
