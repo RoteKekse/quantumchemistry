088794299e4faeb62f3ac3bde59eb875a3267878
Micha <goette@math.tu-berlin.de>
2020-10-21 16:40:59 +0200

    update

--- tests/testekHx.cpp
+++ tests/testekHx.cpp
@@ -28,27 +28,33 @@ int main(){
 	XERUS_LOG(info, "linear " << builder.contract_tree());
 
 	builder.reset(hf);
+	builder.reset_psi(phi);
 	builder.preparePsiEval();
 	XERUS_LOG(info, "tree " << builder.contract_tree());
 
 
-//	std::vector<size_t> linear1;
-//	std::vector<size_t> linear2;
-//	std::vector<size_t> tree;
-//	for (size_t rank : {5,10,20,40,80,150,300,600}){
-//		TTTensor phi = TTTensor::random(std::vector<size_t>(d,2),std::vector<size_t>(d-1,rank));
-//
-//		ContractPsiHek builder(phi,d,p,path_T,path_V,0.0, shift,hf);
-//
-//
-//		builder.reset(hf);
-//		tree.emplace_back(builder.preparePsiEval());
-//		linear1.emplace_back(p*(d-p)*(3*d*p-2*d-3*p*p+4)/32*d*rank*rank);
-//		linear2.emplace_back(builder.preparePsiEval_linear());
-//	}
-//	XERUS_LOG(info, "tree " << tree);
-//	XERUS_LOG(info, "line1 " << linear1);
-//	XERUS_LOG(info, "line2 " << linear2);
+	std::vector<size_t> linear1;
+	std::vector<size_t> linear2;
+	std::vector<size_t> tree;
+	for (size_t rank : {5,10,20,40,80,150,300,600}){
+		TTTensor phi = TTTensor::random(std::vector<size_t>(d,2),std::vector<size_t>(d-1,rank));
+
+		ContractPsiHek builder(phi,d,p,path_T,path_V,0.0, shift,hf);
+
+
+		builder.reset(hf);
+		builder.reset_psi(phi);
+		tree.emplace_back(builder.preparePsiEval());
+
+		linear1.emplace_back(p*(d-p)*(3*d*p-2*d-3*p*p+4)/32*d*rank*rank);
+
+		builder.reset(hf);
+		builder.reset_psi(phi);
+		linear2.emplace_back(builder.preparePsiEval_linear());
+	}
+	XERUS_LOG(info, "tree " << tree);
+	XERUS_LOG(info, "line1 " << linear1);
+	XERUS_LOG(info, "line2 " << linear2);
 //	for (size_t i = 0; i < tree.size(); ++i)
 //		XERUS_LOG(info, value_t (tree[i]) / value_t (linear1[i]));
 
